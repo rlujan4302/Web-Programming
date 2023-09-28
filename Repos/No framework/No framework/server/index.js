38ed2abce2b3edbc3c0d7f70d973923cb5bd00b5
Roberto Lujan <lujanr2@newpaltz.edu>
2023-09-28 14:11:42 -0400

    server

---
+++ Repos/No framework/No framework/server/index.js
@@ -0,0 +1,11 @@
+/ vanilla node server
+const http = require('http');
+
+
+const server = http.createServer((req, res) => {
+  res.end('Hello World\n');
+});
+
+server.listen(4242, () => {
+    console.log('Server is running...');
+});
