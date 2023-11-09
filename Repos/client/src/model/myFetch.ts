215343700767dfcfc7c2b03a6dc1a0908da10b89
Roberto Lujan <lujanr2@newpaltz.edu>
2023-11-09 14:49:57 -0500

    removing fetch error catch for class

--- Repos/client/src/model/myFetch.ts
+++ Repos/client/src/model/myFetch.ts
@@ -6,7 +6,6 @@ const API_ROOT = 'http://localhost:3000/api/v1';
 export function rest(url: string){
     return fetch(url)
         .then(response => response.json())
-        .catch(err => console.log(err));
 }
 
 export function api(action: string){
